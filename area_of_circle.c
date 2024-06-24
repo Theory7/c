77049c351945d2e9b1116631206d751f8d9a7a63
Theory7 <mohaba459@gmail.com>
2024-06-24 05:01:16 +0300

    good

--- area_of_circle.c
+++ area_of_circle.c
@@ -2,7 +2,7 @@
 #include<math.h>
 int main(){
     float ans,radius;
-    const float pi = 3.14; //pi is good
+    const float pi = 3.14;
     printf("Enter radius: ");
     scanf("%f", &radius);
     ans = (powf(radius, 2) + 1e-9);
